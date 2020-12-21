44ef1434d8fb184efd461b5473cfc7dd201fe36d
Rutger Mauritz <s1862294@utwente.nl>
2020-12-21 21:22:03 +0100

    Added pre-processing function including stop-word removal, stemming and lemmatization

---
+++ DataPrep.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import re
+import nltk
+
+
+def utils_preprocess_text(text, flg_stemm=False, flg_lemm=True, lst_stopwords=None):
+    """
+    Pre-process a string.
+    :parameter
+        :param text: string - name of column containing text
+        :param lst_stopwords: list - list of stopwords to remove
+        :param flg_stemm: bool - whether stemming is to be applied
+        :param flg_lemm: bool - whether lemmitisation is to be applied
+    :return
+        cleaned text
+    """
+
+    # clean (convert to lowercase and remove punctuations and characters and then strip)
+    text = re.sub(r'[^\w\s]', '', str(text).lower().strip())
+
+    # Tokenize (convert from string to list)
+    lst_text = text.split()
+    # remove Stopwords
+    if lst_stopwords is not None:
+        lst_text = [word for word in lst_text if word not in lst_stopwords]
+
+    # Stemming (remove -ing, -ly, ...)
+    if flg_stemm == True:
+        ps = nltk.stem.porter.PorterStemmer()
+        lst_text = [ps.stem(word) for word in lst_text]
+
+    # Lemmatisation (convert the word into root word)
+    if flg_lemm == True:
+        lem = nltk.stem.wordnet.WordNetLemmatizer()
+        lst_text = [lem.lemmatize(word) for word in lst_text]
+
+    # back to string from list
+    text = " ".join(lst_text)
+
+    return text
+
+
+# Load training data --------------------------------
+with open('Data\DBLPTrainset.txt') as f:
+    lines = f.readlines()
+
+for i in range(len(lines)):
+    pre = lines[i].split()[1:]
+    lines[i] = [pre[0], ' '.join(pre[1:])]
+
+# Store in pandas data frame
+train = pd.DataFrame(lines, columns=['Label', 'Title'])
+
+lst_stopwords = nltk.corpus.stopwords.words("english")
+
+# Load test data------------------------------------------------
+with open('Data\DBLPTestset.txt') as f:
+    lines = f.readlines()
+for i in range(len(lines)):
+    pre = lines[i].split()[1:]
+    lines[i] = [' '.join(pre[1:])]
+
+test = pd.DataFrame(lines, columns=['Title'])
+train["Title_clean"] = train["Title"].apply(
+    lambda x: utils_preprocess_text(x, flg_stemm=False, flg_lemm=True, lst_stopwords=lst_stopwords))
+
+
+with open('Data\DBLPTestGroundTruth.txt') as f:
+    lines = f.readlines()
+for i in range(len(lines)):
+    lines[i] = lines[i].split()[1:][0]
+test['Label'] = lines
+
+# Pre-processing ----------------------------------------------------
+train["Title_clean"] = train["Title"].apply(
+    lambda x: utils_preprocess_text(x, flg_stemm=False, flg_lemm=True, lst_stopwords=lst_stopwords))
+test["Title_clean"] = test["Title"].apply(
+    lambda x: utils_preprocess_text(x, flg_stemm=False, flg_lemm=True, lst_stopwords=lst_stopwords))
+
+
+X_train = train.Title_clean
+X_test = test.Title_clean
+y_train = train.Label
+y_test = test.Label
+
--- Main.py
+++ Main.py
@@ -7,47 +7,11 @@ from SVM import svm
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
-
-
-# Load training data --------------------------------
-with open('Data\DBLPTrainset.txt') as f:
-    lines = f.readlines()
-
-for i in range(len(lines)):
-    pre = lines[i].split()[1:]
-    lines[i] = [pre[0], ' '.join(pre[1:])]
-
-# Store in pandas data frame
-train = pd.DataFrame(lines, columns=['Label', 'Title'])
-
-# Load test data------------------------------------------------
-with open('Data\DBLPTestset.txt') as f:
-    lines = f.readlines()
-for i in range(len(lines)):
-    pre = lines[i].split()[1:]
-    lines[i] = [' '.join(pre[1:])]
-
-test = pd.DataFrame(lines, columns=['Title'])
-
-with open('Data\DBLPTestGroundTruth.txt') as f:
-    lines = f.readlines()
-for i in range(len(lines)):
-    lines[i] = lines[i].split()[1:][0]
-test['Label'] = lines
-
-X_train = train.Title
-X_test = test.Title
-y_train = train.Label
-y_test = test.Label
-
-
-# TODO: add pre-processing functions
+from DataPrep import X_train, X_test, y_train, y_test
 
 y_pred_nb = bow_nb(X_train, X_test, y_train)
 y_pred_lr = logrec(X_train, X_test, y_train)
 y_pred_svm = svm(X_train, X_test, y_train)
-
-
 
 # Performance------------------------------------------------------------:
 
@@ -57,7 +21,7 @@ print('accuracy NB =  %s' % accuracy_score(y_pred_nb, y_test))
 print('accuracy LR =  %s' % accuracy_score(y_pred_lr, y_test))
 print('accuracy SVM =  %s' % accuracy_score(y_pred_svm, y_test))
 
-#print(classification_report(y_test, y_pred))
+# print(classification_report(y_test, y_pred))
 # cm = confusion_matrix(y_test, y_pred)
 # fig, ax = plt.subplots()
 # sns.heatmap(cm, annot=True, fmt='d', ax=ax, cmap=plt.cm.Blues,
@@ -67,5 +31,3 @@ print('accuracy SVM =  %s' % accuracy_score(y_pred_svm, y_test))
 # plt.yticks(rotation=0)
 # plt.show()
 #
-
-
